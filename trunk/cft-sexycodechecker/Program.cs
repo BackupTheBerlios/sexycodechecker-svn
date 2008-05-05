24ad752ee54110bfc0517d4a3ce1eb32556f7d86
limacat <limacat@d535dd77-144b-0410-a9d9-8d7b065f66f2>
2008-05-05 12:50:13 +0000

    Forgot out

--- trunk/cft-sexycodechecker/Program.cs
+++ trunk/cft-sexycodechecker/Program.cs
@@ -1,6 +1,13 @@
-﻿namespace Cluefultoys.Sexycodechecker {
+﻿using System;
+
+namespace Cluefultoys.Sexycodechecker {
     class Program {
-        static void Main(string[] args) {
+        static void Main(string[] arguments) {
+            int iParam = 0;
+            foreach (string argument in arguments) {
+                Console.Out.WriteLine("param " + iParam + ": " + argument);
+                iParam++;
+            }
         }
     }
 }
